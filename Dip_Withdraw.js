8adb5fa4ce127140c60e9b2da12f6ce874e09235
profBritto <linuxwindows739@gmail.com>
2023-06-24 20:40:15 +0600

    Transaction page  withdraw limit bugs fixed

--- Dip_Withdraw.js
+++ Dip_Withdraw.js
@@ -4,6 +4,9 @@ const btnDeposit=document.getElementById('btnDeposit');
 btnDeposit.addEventListener('click',function(){
     var totalDepo=parseFloat(document.getElementById("totalDepo").innerText);
     var DepositAmmount=parseFloat(document.getElementById("DepositAmmount").value);
+    if (DepositAmmount>=0) {
+        
+    
     totalDepo=totalDepo+DepositAmmount;
     console.log(totalDepo);
     document.getElementById("totalDepo").innerHTML = totalDepo ;
@@ -11,7 +14,11 @@ btnDeposit.addEventListener('click',function(){
     var totalBal=parseFloat(document.getElementById("totalBal").innerText);
     totalBal=totalBal+DepositAmmount;
     console.log(totalBal);
-    document.getElementById("totalBal").innerText=totalBal;   
+    document.getElementById("totalBal").innerText=totalBal; 
+    }else{
+        alert("Enter a valid amount");
+
+    }  
  });
 
 
@@ -23,13 +30,24 @@ btnWithdraw.addEventListener('click',function(){
     if(totalBal>0){
     var totalWithdraw=parseFloat(document.getElementById("totalWithdraw").innerText);
     var WithdrawAmmount=parseFloat(document.getElementById("WithdrawAmmount").value);
-    totalWithdraw=totalWithdraw+WithdrawAmmount;
-    console.log(totalWithdraw);
-    document.getElementById("totalWithdraw").innerHTML = totalWithdraw ;
-
-    totalBal=totalBal-WithdrawAmmount;
-    console.log(totalBal);
-    document.getElementById("totalBal").innerText=totalBal; 
+    if (WithdrawAmmount>=0) {
+        
+        totalWithdraw=totalWithdraw+WithdrawAmmount;
+        console.log(totalWithdraw);
+        document.getElementById("totalWithdraw").innerHTML = totalWithdraw ;
+        
+        totalBal=totalBal-WithdrawAmmount;
+        if(totalBal<0){
+            totalBal=totalBal+WithdrawAmmount;
+            totalWithdraw=totalWithdraw-WithdrawAmmount;
+            document.getElementById("totalWithdraw").innerHTML = totalWithdraw ;
+            alert("Please Change the Ammount!");
+        }
+        console.log(totalBal);
+        document.getElementById("totalBal").innerText=totalBal; 
+    }else{
+        alert("Enter a valid amount");
+    }
     }else{
         document.getElementById("totalBal").innerText=0;
         alert("You have no balance to withdraw");
